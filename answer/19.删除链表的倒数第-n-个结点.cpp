8c4e0c07d700e747fb0523e762b1ac46815369ff
Guomw <isguo_mw@163.com>
2023-05-07 21:07:18 +0800

    代码随想录-链表

--- answer/19.删除链表的倒数第-n-个结点.cpp
+++ answer/19.删除链表的倒数第-n-个结点.cpp
@@ -22,10 +22,13 @@ public:
 
         ListNode *vhead = new ListNode(-1, head);
         ListNode *p1 = vhead;
+        // p1先走n+1步，到达n+1，从虚拟结点开始，就当从0开始
         for (int i = 0; i < n + 1; i++) p1 = p1->next;
 
-        
-
+        // 接着p2随着p1一起走
+        // 最终到达的位置为len-n
+        // 倒数第n个结点是len-n+1
+        // 所以p2指向被删除节点的前一个结点
         ListNode *p2 = vhead;
         while (p1 != nullptr) {
             p1 = p1->next;
