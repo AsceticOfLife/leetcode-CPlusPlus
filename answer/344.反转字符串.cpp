1a18d7d71a9275221dad4b33ff09b2b7ac3fc513
Guomw <isguo_mw@163.com>
2023-05-14 21:52:44 +0800

    代码随想录-字符串

--- answer/344.反转字符串.cpp
+++ answer/344.反转字符串.cpp
@@ -10,6 +10,7 @@ public:
     void reverseString(vector<char>& s) {
         int low = 0, high = s.size() - 1;
 
+
         while (low < high) {
             swap(s[low], s[high]);
             low++;
---
+++ answer/541.反转字符串-ii.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=541 lang=cpp
+ *
+ * [541] 反转字符串 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        for (int i = 0; i < s.size(); i += (2 * k)) {
+            // 1. 每隔 2k 个字符的前 k 个字符进行反转
+            // 2. 剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符
+            if (i + k <= s.size()) {
+                reverse(s.begin() + i, s.begin() + i + k );
+            } else {
+                // 3. 剩余字符少于 k 个，则将剩余字符全部反转。
+                reverse(s.begin() + i, s.end());
+            }
+        }
+        return s;
+    }
+};
+// @lc code=end
+
